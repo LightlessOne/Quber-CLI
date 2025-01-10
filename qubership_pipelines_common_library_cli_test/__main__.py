2fc2717afc6366302c98fdca9a8e221ccd7333b7
Igor Lebedev <lebedev.light@gmail.com>
2025-01-10 09:20:24 +0300

    Added another command

--- qubership_pipelines_common_library_cli_test/__main__.py
+++ qubership_pipelines_common_library_cli_test/__main__.py
@@ -1,6 +1,6 @@
 import os, sys, logging,urllib3, click
 
-from qubership_pipelines_common_library_cli_test.test_command import SampleStandaloneExecutionCommand
+from qubership_pipelines_common_library_cli_test.test_command import SampleStandaloneExecutionCommand, CalcCommand
 from qubership_pipelines_common_library.v1.execution.exec_logger import ExecutionLogger
 
 DEFAULT_CONTEXT_FILE_PATH = 'context.yaml'
@@ -18,4 +18,11 @@ def cli():
 @click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
 def __run_test(context_path):
     command = SampleStandaloneExecutionCommand(context_path)
+    command.run()
+
+
+@cli.command("calc")
+@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
+def __calc(context_path):
+    command = CalcCommand(context_path)
     command.run()
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 cd ../TESTS/
 python --version
 python quber_cli.pyz --help
-python quber_cli.pyz run-test && python quber_cli.pyz run-test
+python quber_cli.pyz run-test && python quber_cli.pyz calc --context_path=context2.yaml
 echo DONE!
