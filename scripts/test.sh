ae2a58b920e16bd19f83b924211825f7616a36e9
Lebedev Light <LightlessOne@users.noreply.github.com>
2025-01-31 13:40:32 +0300

    Merge pull request #1 from LightlessOne/rnd/in_memory_context
    Test new way of triggering ExecutionCommands with ad-hoc context

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,8 @@ python quber_cli --help
 python quber_cli run-sample
 python quber_cli calc --context_path=context2.yaml
 
+python quber_cli umbrella-test
+cd RESULTS_FOLDER
+find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
+
 echo Tests done!
